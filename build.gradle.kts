d4ee01139d59876b9097381805e45157b2a16594
Jiwoo Seo <sjwoos123@gmail.com>
2025-07-03 12:03:47 +0900

    base에서 가져온 내용 스쿼시 병합

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ repositories {
 }
 
 dependencies {
+<<<<<<< HEAD
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -33,6 +34,23 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+=======
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.h2database:h2")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+>>>>>>> c1343cd5e734b4dbe3f32d6ce8cdb2bca2e7f1b6
 }
 
 tasks.withType<Test> {
