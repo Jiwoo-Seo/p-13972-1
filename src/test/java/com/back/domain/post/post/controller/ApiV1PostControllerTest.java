557446fe87f302db461261e97326f841ac383e86
Jiwoo Seo <sjwoos123@gmail.com>
2025-07-04 14:24:53 +0900

    step2

--- src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
+++ src/test/java/com/back/domain/post/post/controller/ApiV1PostControllerTest.java
@@ -73,10 +73,13 @@ public class ApiV1PostControllerTest {
     @Test
     @DisplayName("글 쓰기, without title")
     void t7() throws Exception {
+        Member actor = memberService.findByUsername("user1").get();
+        String actorApiKey = actor.getApiKey();
         ResultActions resultActions = mvc
                 .perform(
                         post("/api/v1/posts")
                                 .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", "Bearer " + actorApiKey)
                                 .content("""
                                         {
                                             "title": "",
@@ -100,10 +103,13 @@ public class ApiV1PostControllerTest {
     @Test
     @DisplayName("글 쓰기, without content")
     void t8() throws Exception {
+        Member actor = memberService.findByUsername("user1").get();
+        String actorApiKey = actor.getApiKey();
         ResultActions resultActions = mvc
                 .perform(
                         post("/api/v1/posts")
                                 .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", "Bearer " + actorApiKey)
                                 .content("""
                                         {
                                             "title": "제목",
@@ -127,7 +133,8 @@ public class ApiV1PostControllerTest {
     @Test
     @DisplayName("글 쓰기, with wrong json syntax")
     void t9() throws Exception {
-
+        Member actor = memberService.findByUsername("user1").get();
+        String actorApiKey = actor.getApiKey();
         String wrongJsonBody = """
                 {
                     "title": 제목",
@@ -138,6 +145,7 @@ public class ApiV1PostControllerTest {
                 .perform(
                         post("/api/v1/posts")
                                 .contentType(MediaType.APPLICATION_JSON)
+                                .header("Authorization", "Bearer " + actorApiKey)
                                 .content(wrongJsonBody)
                 )
                 .andDo(print());
@@ -184,10 +192,14 @@ public class ApiV1PostControllerTest {
     @DisplayName("글 삭제")
     void t3() throws Exception {
         int id = 1;
+        Post post = postService.findById(id).get();
+        Member actor = post.getAuthor();
+        String actorApiKey = actor.getApiKey();
 
         ResultActions resultActions = mvc
                 .perform(
                         delete("/api/v1/posts/" + id)
+                                .header("Authorization", "Bearer " + actorApiKey)
                 )
                 .andDo(print());
 
