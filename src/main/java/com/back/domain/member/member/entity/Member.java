1d1d37c52de27359ebe3a6a390addf7d5197e106
Jiwoo Seo <sjwoos123@gmail.com>
2025-07-09 15:37:37 +0900

    step4

--- src/main/java/com/back/domain/member/member/entity/Member.java
+++ src/main/java/com/back/domain/member/member/entity/Member.java
@@ -40,8 +40,14 @@ public class Member extends BaseEntity {
         this.nickname = name;
     }
 
-
     public void modifyApiKey(String apiKey) {
         this.apiKey = apiKey;
+    }
+
+    public boolean isAdmin() {
+        if ("system".equals(username)) return true;
+        if ("admin".equals(username)) return true;
+
+        return false;
     }
 }
